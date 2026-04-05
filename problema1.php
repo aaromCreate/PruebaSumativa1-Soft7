5c4734adbcf3a656719a14df99ee955790ea250d
aaromCreate <aarom.ortiz4@gmail.com>
2026-04-05 14:58:16 -0500

    Subida completa

---
+++ problema1.php
@@ -0,0 +1,57 @@
+<?php
+// suma
+
+$numero1 = 5;
+
+$numero2 = 3;
+
+$resultado1 = $numero1 + $numero2; 
+
+echo "\nSuma de 5 y 3 es = ".$resultado1; // $resultado contendrá 8
+
+// resta
+
+$numero1 = 10;
+
+$numero2 = 4;
+
+$resultado2 = $numero1 - $numero2; 
+echo "\nResta de 10 y 4 es = ".$resultado2; // $resultado contendrá 6
+
+//Multiplicación
+
+$numero1 = 7;
+
+$numero2 = 3;
+
+$resultado3 = $numero1 * $numero2; 
+echo "\nMultiplicación de 7 y 3 es = ".$resultado3; // $resultado contendrá 21
+
+//División
+
+$numero1 = 15;
+
+$numero2 = 5;
+
+$resultado4 = $numero1 / $numero2; 
+echo "\nDivisión de 15 y 5 es = ".$resultado4;// $resultado contendrá 3
+
+//Módulo
+
+$numero1 = 10;
+
+$numero2 = 3;
+
+$resultado5 = $numero1 % $numero2; 
+echo "\nMódulo de 10 y 3 es = ".$resultado5; // $resultado contendrá 1
+
+//Potencia
+
+$base = 2;
+
+$exponente = 3;
+
+$resultado6 = $base ** $exponente; 
+echo "\nPotencia de 2 y 3 es = ".$resultado6; // $resultado contendrá 8
+
+?>
---
+++ problema2.php
@@ -0,0 +1,12 @@
+<?php
+//Orden de Precedencia
+
+$resultado1 = 2 + 3 * 4;
+echo "\n Resultado de 2 + 3 * 4 es = ".$resultado1;
+
+//Primero se evalúa la multiplicación (3 * 4), y luego se suma el resultado a 2 (2 + 12), lo que da como resultado $resultado = 14.
+
+$resultado2 = (2 + 3) * 4;
+echo "\n Resultado de (2 + 3) * 4 es =  ".$resultado2;
+
+?>
